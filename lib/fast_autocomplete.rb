303decafe1acb1e17949787d4bf22eb4d81d8cbc
Rocketegg <rocketegg@gmail.com>
2016-02-07 21:29:01 -0800

    remove

--- lib/fast_autocomplete.rb
+++ lib/fast_autocomplete.rb
@@ -6,7 +6,6 @@ require "fast_autocomplete/int_set/int_set_hash"
 require "fast_autocomplete/trie"
 require "fast_autocomplete/node"
 require "fast_autocomplete/autocompleter"
-require 'ruby-prof'
 
 module FastAutocomplete
   # Your code goes here...
